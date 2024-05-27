0454fb5782e74abacba7b14d32a9a22978dfb681
tulerws <joaotuler88@gmail.com>
2024-05-27 17:26:34 -0300

    update comment

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 // O NOME DA DISCIPLINA, CODIGO DA TURMA E O NOME E MATRICULA ESTÁ NO README.MD DO REPOSITORIO
 
-function isPrime(num) { //função para 
+// Função para verificar se um número é primo
+function isPrime(num) {
     if (num <= 1) {
         return false;
     }
-    for (let i = 2; i <= num/2; i++) {
-        if (num % i === 0) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {         //sobre a função math.sqrt eu procurei como usar ela 
+        if (num % i === 0) {                            //na internet pra conseguir usar direito no codigo
             return false;
         }
     }
     return true;
 }
 
+// Encontra os 10 maiores números primos a partir de um número dado
 function findLargestPrimes() {
     const input = parseInt(prompt("Digite um número:"));
     const largestPrimes = [];
@@ -22,15 +24,15 @@ function findLargestPrimes() {
         }
     }
 
-    console.log("Os 10 maiores números primos a partir de", input, "são:");
+    console.log("Os 10 maiores números primos a partir de", input, "são:"); //retorno dos 10 maiores primos
     console.log(largestPrimes);
 }
 
-
+// Verifica se todos os números positivos até o valor inserido são primos
 function verificarPrimos() {
     const userInput = parseInt(prompt("Digite um número positivo:"));
     if (userInput <= 0) {
-        console.log("Por favor, digite um número positivo válido.");
+        console.log("Por favor, digite um número positivo válido."); //se for menor ou igual a zero fiz retornar um erro
         return;
     }
 
@@ -42,3 +44,6 @@ function verificarPrimos() {
         }
     }
 }
+
+// Chama a função para encontrar os 10 maiores números primos
+findLargestPrimes();
